d8a24963d41fd118083c6301a0fb8e6b6d78f724
Luis Alberto Jimenez Soto <82974706+Lajs5257@users.noreply.github.com>
2023-01-12 13:41:33 -0600

    Update spotify.py

--- api/spotify.py
+++ api/spotify.py
@@ -166,7 +166,7 @@ def catch_all(path):
     resp = Response(svg, mimetype="image/svg+xml")
     resp.headers["Cache-Control"] = "s-maxage=1"
 
-    return svg
+    return resp
 
 
 if __name__ == "__main__":
